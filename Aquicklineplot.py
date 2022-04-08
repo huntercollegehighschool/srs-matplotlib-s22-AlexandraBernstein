421b6b7ce477ac7017d7858fac17a043fd50fbb8
AlexandraBern <79848052+AlexandraBernstein@users.noreply.github.com>
2022-04-08 16:25:19 +0000

    not much

--- Aquicklineplot.py
+++ Aquicklineplot.py
@@ -2,14 +2,15 @@
 1. Import matplotlib using the code below. (as plt is optional, but is standard)
 import matplotlib.pyplot as plt
 """
-
+import matplotlib.pyplot as plt
 
 """
 2. Use plt.plot() on the list below to create a plot with those four data points.
 plt.plot(<data>)
 """
-
 sample = [1, 3, 4, 2]
+plot = plt.plot(sample)
+
 
 
 """
